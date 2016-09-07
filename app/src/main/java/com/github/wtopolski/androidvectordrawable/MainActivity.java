484668440fc66ffc92825eeb0011e01789db0d5a
Wojciech Topolski <wojciech.topolski@10clouds.com>
2016-09-07 10:55:45 +0200

    Traditional solution for drawables

--- app/src/main/java/com/github/wtopolski/androidvectordrawable/MainActivity.java
+++ app/src/main/java/com/github/wtopolski/androidvectordrawable/MainActivity.java
@@ -1,7 +1,18 @@
 package com.github.wtopolski.androidvectordrawable;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView image = (ImageView) findViewById(R.id.paint);
+        image.setImageBitmap(generateIcon());
+    }
+
+    public void onSelectMe(View view) {
+        CheckBox checkbox = (CheckBox) view;
+        Toast.makeText(MainActivity.this, "" + checkbox.isChecked(), Toast.LENGTH_SHORT).show();
+    }
+
+    private Bitmap generateIcon() {
+        DisplayMetrics metrics = getApplicationContext().getResources().getDisplayMetrics();
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_custom);
+
+        Bitmap alteredBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
+
+        Canvas canvas = new Canvas(alteredBitmap);
+        canvas.drawBitmap(bitmap, 0, -8, new Paint());
+
+        Paint paintText = new Paint();
+        paintText.setAntiAlias(true);
+        paintText.setTextAlign(Paint.Align.CENTER);
+        paintText.setColor(ContextCompat.getColor(this, R.color.colorBackground));
+        paintText.setTextSize((int) (7 * metrics.density));
+        paintText.setTypeface(Typeface.DEFAULT_BOLD);
+        canvas.drawText("JOHN", bitmap.getWidth() / 2f, bitmap.getHeight() / 1.1f, paintText);
+
+        bitmap.recycle();
+
+        return alteredBitmap;
     }
 }
